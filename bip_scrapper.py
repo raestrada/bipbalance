f7afc5c6b73df250b9811fb9ec5a0fb3eda1f673
Rodrigo Estrada <rodrigo.estrada@gmail.com>
2015-08-30 01:37:06 -0400

    Refactoring the scrapper to be used as function and exposing as a REST Service

--- bip_scrapper.py
+++ bip_scrapper.py
@@ -2,19 +2,20 @@ from ghost import Ghost
 
 bip_balance_url = 'http://pocae.tstgo.cl/PortalCAE-WAR-MODULE/'
 
-if __name__ == '__main__':
+def get_balance(card_id):
     ghost = Ghost()
-    browser = ghost.start()
 
     with ghost.start() as browser:
-        page, resources = browser.open(bip_balance_url)
+        browser.open(bip_balance_url)
 
-        result, resources = browser.set_field_value("input[id=txtNumTarjeta]", "14219394")
-        page, resources = browser.click("input[id=btnEnviar]", expect_loading=True)
+        browser.set_field_value("input[id=txtNumTarjeta]", card_id)
+        browser.click("input[id=btnEnviar]", expect_loading=True)
 
         balance_amount, resources = browser.evaluate('document.evaluate("//*[contains(text(), \'Saldo  tarjeta:\')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.nextSibling.nextSibling.innerHTML;')
         balance_date, resources = browser.evaluate('document.evaluate("//*[contains(text(), \'Fecha saldo: \')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.nextSibling.nextSibling.innerHTML;')
 
-        balance = { 'amount': balance_amount, 'date': balance_date }
+        return {'amount': balance_amount, 'date': balance_date}
 
-        print balance
+if __name__ == '__main__':
+    print get_balance('14219394')
+
